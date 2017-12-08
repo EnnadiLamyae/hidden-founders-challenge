efbb60dc7f87f5a72b485e527411dd50f75e8fc2
ennadiLamyae <ennadi.lamyae@gmail.com>
2017-12-08 15:40:25 +0000

    config

--- plugins/fb-config.js
+++ plugins/fb-config.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 Vue.prototype.$initFbSDK = () => {
   FB.init({
-    appId: 1536641103081775,
+    //this test app 
+    appId: appId,
     cookie: true,
     xfbml: true,
     version: 'v2.11'
--- plugins/firebase-config.js
+++ plugins/firebase-config.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'
   // Initialize Firebase
   let config = {
-    apiKey: "AIzaSyBVRd_HaSy-RTHuBIgRWDN9Bf_xNT1DLuQ",
-    authDomain: "lamyaechallenge.firebaseapp.com",
-    databaseURL: "https://lamyaechallenge.firebaseio.com",
-    projectId: "lamyaechallenge",
-    storageBucket: "lamyaechallenge.appspot.com",
-    messagingSenderId: "737038420875"
+    apiKey: "app-key",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
   firebase.initializeApp(config)
 let database = firebase.database()
